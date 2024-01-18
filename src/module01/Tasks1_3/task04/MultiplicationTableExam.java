349e08b52a36b1618ab6b310f175a35a7df0159c
Jafestro <jafestro@gmail.com>
2024-01-18 10:44:38 +0200

    Multiplication exam

--- src/module01/Tasks1_3/task04/MultiplicationTableExam.java
+++ src/module01/Tasks1_3/task04/MultiplicationTableExam.java
@@ -8,16 +8,22 @@ public class MultiplicationTableExam {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
 
-        int first, second;
+        int first, second, answer;
 
         System.out.println("**************WELCOME TO MULTIPLICATION EXAM!**************");
 
         for (int i = 1; i <= 10 ; i++) {
             first = random.nextInt(10) + 1;
             second = random.nextInt(10) + 1;
-            System.out.printf("1)  %s * %s = ?\n", first, second);
+            System.out.printf("%s)  %s * %s = ", i, first, second);
 
-            Scanner
+            answer = Integer.parseInt(scanner.nextLine());
+
+            if (first * second == answer){
+                System.out.println("CORRECT!");
+            }else {
+                System.out.println("Not Correct, next time you will get it right:)");
+            }
         }
     }
 }
