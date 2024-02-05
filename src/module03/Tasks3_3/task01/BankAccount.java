5353195c8afbba4e1ec86d403ae5fc57466dbe66
Jafestro <jafestro@gmail.com>
2024-02-05 14:16:39 +0200

    Task4 module3.3

---
+++ src/module03/Tasks3_3/task01/BankAccount.java
@@ -0,0 +1,47 @@
+package module03.Tasks3_3.task01;
+
+public class BankAccount {
+    // Your variable declarations and code here
+    private int accountNumber;
+    private double balance;
+    private static int totalAccounts = 0;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+        totalAccounts++;
+        accountNumber = totalAccounts;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(2000);
+
+        account1.deposit(500);
+        account2.withdraw(800);
+
+        System.out.println("Account " + account1.getAccountNumber() + " balance: " + account1.getBalance());
+        System.out.println("Account " + account2.getAccountNumber() + " balance: " + account2.getBalance());
+
+        System.out.println("Total number of accounts: " + BankAccount.getTotalAccounts());
+    }
+}
+
