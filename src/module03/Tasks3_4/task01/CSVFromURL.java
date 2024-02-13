f17d0700eda485d254ba60ea618133468f508834
Jafestro <jafestro@gmail.com>
2024-02-13 14:54:55 +0200

    Task1 module3.4

--- src/module03/Tasks3_4/task01/CSVFromURL.java
+++ src/module03/Tasks3_4/task01/CSVFromURL.java
@@ -27,21 +27,22 @@ public class CSVFromURL {
             String line;
             double sum = 0;
             int numCount = 0;
+            String startFrom = "01.01.2023";
+            String till = "02.01.2023";
 
             while((line = bufferedReader.readLine()) != null){
-                line = bufferedReader.readLine();
                 String[] list = line.split(";"); // 01.01.2023 00:00;5,0;21,4;25,0;25,5;30,0;21,8;22,1;3,7;44,0;52,1;47,0;18,3;56,1;15,3;59,0;3,9;10,0;21,2;23,7;24,0
-                if(Arrays.asList(list[0].split(" ")).contains("01.01.2023")) { // ["01.01.2023 00:0...", "5,0", "21,4", "25,0", "25,5", +16 more]
+                if(Arrays.asList(list[0].split(" ")).contains(startFrom)) { // ["01.01.2023 00:0...", "5,0", "21,4", "25,0", "25,5", +16 more]
                     String rawNum = list[1].replace(',', '.'); // 5,0 -> 5.0
                     double num = Double.parseDouble(rawNum);
                     sum += num;
                     numCount++;
-                } else if (Arrays.asList(list[0].split(" ")).contains("02.01.2023")) {
+                } else if (Arrays.asList(list[0].split(" ")).contains(till)) {
                     break;
                 }
             }
             double roundedAverage = Math.round((sum/numCount) * 100.0) / 100.0;
-            System.out.println("Average temperature for 1st of January 2023 is " + roundedAverage + " degrees in Celsius");
+            System.out.println("Average temperature for " + startFrom + " is " + roundedAverage + " degrees in Celsius");
             bufferedReader.close();
         }catch (IOException e){
             throw new RuntimeException();
