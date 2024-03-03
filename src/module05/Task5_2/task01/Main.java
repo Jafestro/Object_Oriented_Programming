709641ae602a1d2de448a1612e06d3d108f908a8
Jafestro <jafestro@gmail.com>
2024-03-03 07:53:18 +0200

    module 5.2  task 1

---
+++ src/module05/Task5_2/task01/Main.java
@@ -0,0 +1,20 @@
+package module05.Task5_2.task01;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Reservation reservation = new Reservation(10);
+        new Thread(new Customer(1, reservation)).start();
+        new Thread(new Customer(2, reservation)).start();
+        new Thread(new Customer(4, reservation)).start();
+        new Thread(new Customer(3, reservation)).start();
+        new Thread(new Customer(1, reservation)).start();
+        new Thread(new Customer(2, reservation)).start();
+        new Thread(new Customer(4, reservation)).start();
+        new Thread(new Customer(3, reservation)).start();
+        new Thread(new Customer(1, reservation)).start();
+        new Thread(new Customer(2, reservation)).start();
+        new Thread(new Customer(4, reservation)).start();
+        new Thread(new Customer(3, reservation)).start();
+
+    }
+}
